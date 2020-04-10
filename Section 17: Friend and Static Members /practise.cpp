3057d07fe961298cd183eb055ac47ca857710f02
xinrun <zhan3031@pnw.edu>
2020-04-09 23:06:01 -0500

    update section 17

---
+++ Section 17: Friend and Static Members /practise.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Test2;
+
+class Test{
+    public: 
+        Test(int n=10){
+            a = n;
+        }
+
+        friend int getA(); // friend function
+        friend Test2; // friend class
+
+    private:
+        int a = 0;    
+};
+
+int getA(){
+    Test t(5);
+    return t.a;
+}
+
+class Test2{
+    public:
+        Test b;
+
+        void fun(){
+            cout<<"friend class: "<<b.a<<endl;
+        }
+};
+
+int main(){
+    cout<<"Hello~"<<endl;
+    int t = getA();
+    cout<< "friend function: "<<t<<endl;
+
+    Test2 kk;
+    kk.fun();
+
+    return 0;
+}
