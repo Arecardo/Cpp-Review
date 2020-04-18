f47690bd79d71c2704b817648a3defbc6b7ae3c5
xinrun <zhan3031@pnw.edu>
2020-04-18 15:12:14 -0500

    update test

--- test/test.cpp
+++ test/test.cpp
@@ -5,6 +5,18 @@ using namespace std;
 int main(){
     string a = "ab";
     string b = "acdb";
+    
+    const int i = 1;
+
+    // copy, top-level const is ignored
+    int j = i;
+    const int* const p1 = &j;
+    j = 2;
+    cout<<"The value of j is: "<<j<<endl;
+
+    // copy, low-level const cannot be ignored
+    // top-level const is ignored
+    const int *p2 = p1;
 
     if(isPredecessor(a, b)) cout<<"Yes"<<endl;
     else cout<<"No"<<endl;
